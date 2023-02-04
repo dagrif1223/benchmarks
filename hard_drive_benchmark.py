076ecd824818fdf87be55df252fee7539fe52b37
dagrif1223 <102176181+dagrif1223@users.noreply.github.com>
2023-02-03 23:13:05 -0500

    created a function for reading the file and outputting total execution time

--- hard_drive_benchmark.py
+++ hard_drive_benchmark.py
@@ -8,3 +8,13 @@ def write_file():
         f.write(b'\xAB'*100)
         
 write_file()
+
+def read_file():
+  with open('file.txt', 'r') as f:
+    for i in range(10000):
+      for j in range(1000):
+        a = f.read(100)
+        
+read_file()
+end = time.time()
+print('The total time in sec for hard drive benchmark 1 was', end-start)
