670b485f9e8c6e2388d87966443c6d45293fd529
dagrif1223 <102176181+dagrif1223@users.noreply.github.com>
2023-02-03 22:57:24 -0500

    created a function for float division as well as outputted the total time of the benchmark

--- float_benchmark.py
+++ float_benchmark.py
@@ -18,3 +18,15 @@ def float_mult():
       c = a * b
  
 float_mult()
+
+def float_div():
+  a = 1.0
+  b = 2.0
+  for i in range(200000):
+    for j in range(10000):
+      c = a/b
+      
+float_div()
+
+end = time.time()
+print('The total time for the float benchmark was', end-start)
